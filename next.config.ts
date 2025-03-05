71e9f4c7aaed408641ec53e44c8ffbfeac158cd4
oguzilgar <ouz1571@gmail.com>
2025-03-05 21:15:42 +0100

    fifth commit

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    esmExternals: "loose", // Allows ESM modules
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.mjs$/,
-      type: "javascript/auto", // Ensures Webpack processes ESM files correctly
-    });
-    return config;
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
-
+export default nextConfig;
