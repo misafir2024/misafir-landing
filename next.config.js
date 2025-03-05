89bc8743b5bcddf55708eb26b7dc2c5885764ff6
oguzilgar <ouz1571@gmail.com>
2025-03-05 21:33:18 +0100

    seventth commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     esmExternals: "loose", // Allows ESM modules
   },
+  // Add images configuration to ensure images work properly
+  images: {
+    domains: [],
+    unoptimized: true, // This can help with image loading issues on Vercel
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.mjs$/,
@@ -13,5 +18,7 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
 
 
