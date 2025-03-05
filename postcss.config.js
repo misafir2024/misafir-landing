ff7656273c75859a9ef6209895ed6c0dc8be33cc
oguzilgar <ouz1571@gmail.com>
2025-03-05 21:59:51 +0100

    eleventh  commit

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: {
+      // If you want to use Tailwind, uncomment these lines and install the dependencies
+       'tailwindcss': {},
+       'autoprefixer': {},
+    },
+  }
+  
+  
